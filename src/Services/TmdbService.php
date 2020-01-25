3da267741406b553e2ce5fcff1023ab417e51f2b
Fwdmo <fawdhawimotamarad@gmail.com>
2020-01-25 03:17:42 +0100

    change

--- src/Services/TmdbService.php
+++ src/Services/TmdbService.php
@@ -4,4 +4,18 @@ namespace Vebo\Shasha\Services;
 
 class TmdbService extends AbstractService
 {
+    public function tv()
+    {
+        //
+    }
+
+    public function movie()
+    {
+        //
+    }
+
+    public function get()
+    {
+        //
+    }
 }
