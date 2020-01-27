9029047a14ce575e1f2b312fccc003dec963633c
Fwdmo <fawdhawimotamarad@gmail.com>
2020-01-27 19:06:27 +0100

    change

--- src/Services/AbstractService.php
+++ src/Services/AbstractService.php
@@ -2,8 +2,22 @@
 
 namespace Vebo\Shasha\Services;
 
+use Vebo\Shasha\Configration;
 use Vebo\Shasha\Contracts\ServiceContract;
 
 abstract class AbstractService implements ServiceContract
 {
+    protected $base_url;
+
+    protected $url;
+
+    public function __construct($name, array $config) {
+
+        $this->config = new Configration($config);
+    }
+
+    public function getBaseUrl()
+    {
+        return $this->base_url;
+    }
 }
